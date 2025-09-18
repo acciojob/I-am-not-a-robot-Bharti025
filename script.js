35005ae1321c70b9826ef48019bb549cd6cabe17
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-09-18 14:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+let image=document.getElementById("random");
+let number=Math.floor(Math.random()*5)+1;
+image.setAttribute("class","img"+number);
+let images=document.getElementsByTagName("img");
+let reset=document.getElementById("reset");
+let reset=document.getElementById("verify");
+reset.style.display="none";
+verify.style.display="none";
+let arr=[];
+for(let i=0; i<images.length; i++){
+	images[i].addEventListener("click",()=>{
+		arr.push(i);
+		if(arr.length==1){
+			reset.style.display="block";
+		}
+		if(arr.length==2){
+			verify.style.display="block";
+		}
+	})
+}
+
+
+
+
+
+
